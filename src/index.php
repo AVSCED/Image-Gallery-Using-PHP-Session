ba1769183f102a4aee96205e193c73e11f60584a
AVSCED <anantvikramshukla2022sep@cedcoss.com>
2022-10-26 16:25:54 +0530

    Gallery Using PHP Session

--- src/index.php
+++ src/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 if (isset($_POST['resetData'])) {
     session_destroy();
@@ -7,55 +7,66 @@ if (isset($_POST['resetData'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Upload</title>
     <style>
-.button1 {
-  background-color: #0099ff;
-  border-radius: 8px ;
-  border: 1px , solid ;
-  color: white;
-  padding: 4px 9px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 7px 5px;
-}
-.row1 {
-    display: flex;
-}
-img{
-    margin-right: 2rem;
-}
-</style>
+        .button1 {
+            background-color: #0099ff;
+            border-radius: 8px;
+            border: 1px, solid;
+            color: white;
+            padding: 4px 9px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+            font-size: 16px;
+            margin: 7px 5px;
+        }
+
+        .row1 {
+            display: flex;
+        }
+
+        img {
+            margin-right: 2rem;
+        }
+    </style>
 
 </head>
-<?php 
+<?php
 if (!isset($_SESSION['data'])) {
     $_SESSION['data'] = [];
 }
 ?>
+
 <body>
     <h3>Image Gallery !</h3>
     <p>This Page displays the list of uploaded images.</p>
+    <!-- To let user upload various Images and store it in session array -->
+
     <form action="upload.php" method="post" name="img_Gallery" enctype="multipart/form-data">
-    <input style="margin-left:.5% " type="file" name="fileToUpload" id="fileToUpload"><br>
-    <button class="button1" type="submit" value="Upload Image" name="submit">Upload More</button>
-</form>
-<form action="" method="POST">
+        <input style="margin-left:.5% " type="file" name="fileToUpload" id="fileToUpload"><br>
+        <button class="button1" type="submit" value="Upload Image" name="submit">Upload More</button>
+    </form>
+
+    <!-- To reset the session Data -->
+    <form action="" method="POST">
         <p style="margin-left:.5% ">Reset data:
             <button type=submit name="resetData">&#9850;</button>
         </p>
     </form>
-<hr><br>
-<?php 
-foreach ($_SESSION['data'] as $key => $value) {
-    $s.='<div class= "row1"><p><img src="./uploads/'.$value.'" alt="" width="200px" height="150px" ><br>'.$value.'</p><div>'; 
-}echo $s;
-?>
+    <hr><br>
+    <?php
+    // To Display the session Data of images
+    foreach ($_SESSION['data'] as $key => $value) {
+        $s .= '<div class= "row1"><p><img src="./uploads/' . $value . '" alt="" width="200px" height="150px" ><br>' . $value . '</p><div>';
+    }
+    echo $s;
+    ?>
 </body>
+
 </html>
--- src/upload.php
+++ src/upload.php
@@ -1,15 +1,14 @@
 <?php
 session_start();
 
-
+// To store the image name in $var and store the image in upload file
 if (isset($_POST['submit'])) {
-    $fileToUpload=$_FILES["fileToUpload"]["name"];
+    $fileToUpload = $_FILES["fileToUpload"]["name"];
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
-    $var=basename($_FILES["fileToUpload"]["name"]);
-   // $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+    $var = basename($_FILES["fileToUpload"]["name"]);
     move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
-    array_push($_SESSION['data'] , $var);
+    array_push($_SESSION['data'], $var);
     header("Location:index.php");
 }
